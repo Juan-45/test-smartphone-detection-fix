2cf1daa67b4c1f3f42c0224a14b49cc9cb3b6b36
Juan-45 <quijuan12@gmail.com>
2022-01-07 14:42:58 -0300

    Add check logic for touch screens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect, useCallback } from "react";
 
 function App() {
+  const [isMobile, setIsMobile] = useState();
+
+  const checkIfIsMobile = useCallback(() => {
+    try {
+      window.document.createEvent("TouchEvent");
+      setIsMobile(true);
+    } catch (e) {
+      setIsMobile(false);
+    }
+  }, []);
+
+  useEffect(() => checkIfIsMobile(), [checkIfIsMobile]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {isMobile ? "Estas usando un celular" : "No estas usando un celular"}
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
